8f8a9cb80cf35b3072943a3e0b8f1e646b482c3e
eternal-silence00 <andron555666777@gmail.com>
2026-05-10 21:14:44 +0300

    feat: add task repository

---
+++ app/repositories/task.py
@@ -0,0 +1,40 @@
+from sqlalchemy import select
+from sqlalchemy.ext.asyncio import AsyncSession
+from app.models.task import Task
+from app.schemas.task import TaskPatch, TaskCreate
+
+class TaskRepo:
+    def __init__(self, session: AsyncSession):
+        self.session = session
+        
+    async def get_by_id(self, task_id: int):
+        result = await self.session.execute(select(Task).where(Task.id == task_id))
+        return result.scalar_one_or_none()
+    
+    async def get_all_users_tasks(self, user_id: int):
+        result = await self.session.execute(select(Task).where(Task.user_id == user_id))
+        return result.scalars().all()
+    
+    async def patch_tasks(self, task_id, data: TaskPatch):
+        result = await self.session.execute(select(Task).where(Task.id == task_id))
+        task = result.scalar_one_or_none()
+        updates = data.model_dump(exclude_unset=True)
+        for key, value in updates.items():
+            setattr(task, key, value)
+        return task 
+    
+    async def create_task(self, data: TaskCreate, user_id: int):
+        task = Task(user_id = user_id,
+                    title = data.title, 
+                    description = data.description)
+        self.session.add(task)
+        await self.session.flush()
+        await self.session.refresh(task)
+        return task
+    
+    async def delete_task(self, task_id: int):
+        task_to_del = await self.session.execute(select(Task).where(Task.id == task_id))
+        task = task_to_del.scalar_one_or_none()
+        await self.session.delete(task)
+        await self.session.flush()
+        return 
